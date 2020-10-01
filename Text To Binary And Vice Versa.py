c6a0522d0f5f68b4c2bfb04650115305735cb6b3
Harish795 <67597538+Harish795@users.noreply.github.com>
2020-10-01 09:44:50 +0530

    Improvised The Software
    Improvised The Software and And Optimized For Performance

--- Text To Binary And Vice Versa.py
+++ Text To Binary And Vice Versa.py
@@ -1,7 +1,21 @@
-import binascii, sys, time
+from binascii import hexlify, unhexlify
+from sys import exit 
+from time import sleep 
+from colorama import Fore, init, Back
+print(Fore.GREEN'''       
+ _______        _ _______    ____  _                                                      
+|__   __|      | |__   __|  |  _ \(_)                               
+   | | _____  _| |_ | | ___ | |_) |_ _ __   __ _ _ __ _   _   
+   | |/ _ \ \/ / __|| |/ _ \|  _ <| | '_ \ / _` | '__| | | | 
+   | |  __/>  <| |_ | | (_) | |_) | | | | | (_| | |  | |_| |
+   |_|\___/_/\_\\__| |_|\___/|____/|_|_| |_|\__,_|_|   \__, |
+                                                       __/ |                                                                 
+                                                      |___/                                                                  
+
+        '''Fore.WHITE)
 
 def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
-    bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
+    bits = bin(int(hexlify(text.encode(encoding, errors)), 16))[2:]
     return bits.zfill(8 * ((len(bits) + 7) // 8))
 
 def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
@@ -11,10 +25,10 @@ def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
 def int2bytes(i):
     hex_string = '%x' % i
     n = len(hex_string)
-    return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
+    return unhexlify(hex_string.zfill(n + (n & 1)))
 
 def to_exit():
-    sys.exit("*"*60+"\nThanks For Using\n"+"*"*60+"")
+    exit("*"*60+"\nThanks For Using\n"+"*"*60+"")
 
 print('\n',"*"*60,'\n'" [0]  Text To Binary"'\n'" [1]  Binary To Text"'\n'" [99] Exit "'\n',"*"*60)
 
@@ -24,7 +38,7 @@ if option_input == '0':
     input_for_binary = input("Enter Your Text Input : ")
     print("*"*60,'\n'"Text   : ",input_for_binary,'\n'"Binary : ",text_to_bits(input_for_binary)) 
     to_exit()
-    time.sleep(10)
+    sleep(10)
      
 elif option_input == '1':
     input_for_text = input("Enter Your Binary Input : ")
@@ -32,9 +46,9 @@ elif option_input == '1':
     to_exit()
     
 elif option_input == '99':
-    sys.exit("Bye")
+    exit("Bye")
 
 else:
     print("*"*60 + "Invalid Input" + "*"*60)
     
-time.sleep(1000000000000000000000000000000000000000000000000000)
+sleep(1000)
